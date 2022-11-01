664ecdbd24bee4a6383aed49e88f36a3813bbf27
BOZKURT <emrebzkurt1@gmail.com>
2022-11-01 23:55:40 +0300

    Initial tests

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -3,13 +3,17 @@ import { site } from "../data/site.ts";
 
 export function HomeHeader() {
   return (
-    <header
-      class="bg-yellow-200 relative min-h-[30vh]"
-    >
+    <header class="bg-yellow-100 relative min-h-[5vh]">
       <Container>
-        <h1 class="text-4xl lg:text-8xl font-bold absolute bottom-6 flex items-center">
-          {site.title}
-        </h1>
+		<div class="relative h-32 w-50 text-4xl"> 
+		  <div class="absolute inset-y-0 left-0 w-50">Mehmet Emre Bozkurt</div>
+	    </div>
+		<div class="relative h-32 w-50 text-4xl">
+  <div class="absolute top-0 right-0 h-16 w-50">CV</div>
+</div>
+<div class="relative h-32 w-50 text-4xl">
+  <div class="absolute bottom-0 right-0 h-16 w-50">LinkedIN</div>
+</div>
       </Container>
     </header>
   );
--- data/site.ts
+++ data/site.ts
@@ -1,5 +1,5 @@
 export const site = {
-  title: 'Fresh Blog',
+  title: 'Mehmet Emre Bozkurt',
   description: 'A blog built with Fresh',
   // This should be a url to the static file you're using for your open graph image
   ogImage: 'https://fresh-blog.littlesticks.dev/og-image.png',
